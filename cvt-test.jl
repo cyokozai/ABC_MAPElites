158a163e4caa7ae982a095e2a5bbeea572cef9ad
cyokozai <yuu4suke27@outlook.jp>
2025-01-05 15:15:19 +0000

    cvt-test.jlでのポリゴンの色付け処理を改善し、cellFitnessの計算を削除してcolormapを直接使用するように変更しました。また、load_polygonの使用を廃止し、load_centroidsを導入しました。

--- cvt-test.jl
+++ cvt-test.jl
@@ -28,9 +28,9 @@ append!(points, [[UPP, UPP], [UPP, LOW], [LOW, UPP], [LOW, LOW]])
 vorn = centroidal_smooth(voronoi(triangulate(points; rng), clip = false); maxiters = 1000, rng = rng)
 save("$(path)CVT-cvttest-$(cvt_vorn_data_update).jld2", "voronoi", vorn)
 
-load_vorn     = load("$(path)CVT-cvttest-$(cvt_vorn_data_update).jld2", "voronoi")
-load_controid = DelaunayTriangulation.get_generators(load_vorn)
-load_polygon  = DelaunayTriangulation.get_polygons(load_vorn)
+load_vorn = load("$(path)CVT-cvttest-$(cvt_vorn_data_update).jld2", "voronoi")
+load_centroids = DelaunayTriangulation.get_generators(load_vorn)
+centroid_values = collect(values(load_centroids))
 
 fig = Figure()
 
@@ -45,36 +45,10 @@ ax = Axis(
 
 resize_to_layout!(fig)
 
-cellFitness = Dict{Int, Float64}(Int(key) => 0.0 for key in keys(load_polygon))
-instances = Dict{Int, Vector{Float64}}()
-colormap = ColorSchemes.viridis
-
-function map_fitness_to_color(fitness::Dict{Int, Float64}, colormap)
-    min_val, max_val = 0.0, 1.0
-    range_val = 1.0 / (length(colormap.colors) - 1)
-    
-    return Dict(key => colormap.colors[round(Int, (value - min_val) / (max_val - min_val) / range_val) + 1] for (key, value) in fitness)
-end
-
-for _ in 1:N
-    instance  = rand(rng, D) .* (UPP - LOW) .+ LOW
-    benchmark = sum((instance .- 0) .^ 2)
-    fitness   = benchmark > 0 ? 1.0 / (1.0 + benchmark) : 1.0 + abs(benchmark)
-    
-    distances = [norm([instance[1] - centroid[1], instance[2] - centroid[2]], 2) for centroid in values(load_polygon)]
-    closest_centroid_index = argmin(distances)
-    
-    cellFitness[closest_centroid_index] = fitness
-    instances[closest_centroid_index]   = instance
-end
-
-colors = map_fitness_to_color(cellFitness, colormap)
-polygon_colors = [colors[i] for i in keys(load_polygon)]
-
 voronoiplot!(
     ax, 
     load_vorn,
-    color = polygon_colors,
+    colormap = :viridis,
     strokewidth = 0.01,
     show_generators = false,
     clip = (LOW, UPP, LOW, UPP)
@@ -88,10 +62,10 @@ Colorbar(
     label    = "Evaluation value"
 )
 
-for (index, instance) in instances
-    scatter!(ax, [instance[1]], [instance[2]], color = :black, markersize = 10)
-    text!(ax, [instance[1]], [instance[2]], text = @sprintf("%.4f", cellFitness[index]), align = (:center, :bottom), color = :black)
-end
+# for (index, instance) in instances
+#     scatter!(ax, [instance[1]], [instance[2]], color = :black, markersize = 10)
+#     text!(ax, [instance[1]], [instance[2]], text = @sprintf("%.4f", cellFitness[index]), align = (:center, :bottom), color = :black)
+# end
 
 resize_to_layout!(fig)
 
