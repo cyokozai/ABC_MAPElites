ffad5c7d01781dab499d3c0c895878a2732c9c3e
cyokozai <yuu4suke27@outlook.jp>
2025-01-07 20:16:40 +0000

    docker-runner.pyとdocker-compose.yamlでのメソッドを"de"から"abc"に変更し、ループ回数を1から2に増加させました。また、docker-compose-run.yamlでのサービス名を更新しました。

--- make-vorn.jl
+++ make-vorn.jl
@@ -14,19 +14,30 @@ include("config.jl")
 
 include("benchmark.jl")
 
-# include("logger.jl")
+include("logger.jl")
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
+dimention = ARGS[1]
 method_name = ARGS[2]
 map_name = ARGS[3]
 function_name = ARGS[4]
-cvtchenge = ARGS[5]
-closeup = 0.2
+cvtchange = ARGS[5]
+
+
+closeup = if function_name == "rastrigin"
+    0.2
+elseif function_name == "rosenbrock"
+    0.2
+elseif function_name == "sphere"
+    0.05
+end
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
-load_path = if ARGS[1] == "test"
+fitness(x::Float64) = x >= 0 ?  1.0 / (1.0 + x) : abs(1.0 + x)
+
+load_path = if dimention == "test"
     global LOW, UPP = -5.12, 5.12
     dir = "./result/testdata/"
     if !isdir(dir)
@@ -39,7 +50,7 @@ else
     if !isdir(dir)
         error("Directory $dir does not exist.")
     end
-    [path for path in readdir(dir) if occursin("CVT-", path) && occursin("$(method_name)-$(map_name)-$(function_name)-", path)]
+    [path for path in readdir(dir) if occursin("CVT-", path) && occursin("$(method_name)-$(map_name)-$(function_name)-$(dimention)", path)]
 end
 
 if isempty(load_path)
@@ -59,8 +70,14 @@ println("Extracted date: ", filedate)
 
 load_vorn = load(loadpath, "voronoi")
 
-filepath = if ARGS[1] == "test"
+individualData = Vector{Tuple{Float64, Float64}}()
+updateCountData = Vector{Int64}()
+fitnessData = Vector{Float64}()
+BestPoint = Vector{Tuple{Float64, Float64}}()
+
+filepath = if dimention == "test"
     dir = "./result/testdata/"
+
     if !isdir(dir)
         error("Directory $dir does not exist.")
     end
@@ -68,89 +85,198 @@ filepath = if ARGS[1] == "test"
     [path for path in readdir(dir) if occursin("test-", path) && occursin("result-", path)]
 else
     dir = "./result/$(method_name)/$(function_name)/"
+    
     if !isdir(dir)
         error("Directory $dir does not exist.")
     end
 
-    [path for path in readdir(dir) if occursin("result-$(filedate)-$(method_name)-$(map_name)-$(function_name)-$(ARGS[1])", path)]
+    [path for path in readdir(dir) if occursin("result-$(method_name)-$(map_name)-$(function_name)-$(dimention)-$(filedate)", path)]
 end
 
-updateCountData = Vector{Int64}()  # Change the type to Vector{Int64}
-BestPoint = Vector{Tuple{Float64, Float64}}()  # Change the type to Vector{Tuple{Float64, Float64}}
+if occursin(".dat", filepath[end])
+    println("Reading: ", joinpath(dir, filepath[end]))
+    
+    open(joinpath(dir, filepath[end]), "r") do io  # Use joinpath to construct the full path
+        reading_data = false # ボーダーライン検出用フラグ
+        border_count = 0  # ボーダーラインのカウント
+        
+        for (k, line) in enumerate(eachline(io)) # ファイルを1行ずつ読み込む
+            if occursin("=", line) # ボーダーラインを検出
+                border_count += 1
 
-for f in filepath
-    if occursin(".dat", f)
-        open(joinpath(dir, f), "r") do io  # Use joinpath to construct the full path
-            reading_data = false # ボーダーライン検出用フラグ
-            border_count = 0  # ボーダーラインのカウント
-            
-            for (k, line) in enumerate(eachline(io)) # ファイルを1行ずつ読み込む
-                if occursin("=", line) # ボーダーラインを検出
-                    border_count += 1
-                    if border_count == 1 # 1つ目のボーダーラインに到達したらデータ読み取り開始
-                        reading_data = true
-                        continue
-                    elseif border_count == 2
-                        reading_data = false
-                        continue
-                    end
+                if border_count == 1 # 1つ目のボーダーラインに到達したらデータ読み取り開始
+                    reading_data = true
+
+                    continue
+                elseif border_count == 2
+                    reading_data = false
+
+                    continue
                 end
+            end
+            
+            if reading_data
+                parsed_value = tryparse(Int64, line)
                 
-                if reading_data
-                    parsed_value = tryparse(Int64, line)
-                    
-                    if parsed_value !== nothing && parsed_value >= 0
-                        push!(updateCountData, parsed_value)  # Use push! to add elements to Data
-                    end
-                elseif occursin("Best behavior:", line)
-                    m = Base.match(r"\[(-?\d+\.\d+),\s*(-?\d+\.\d+)\]", line)  # Use regex to extract two floats
-                    if m !== nothing
-                        x, y = parse(Float64, m.captures[1]), parse(Float64, m.captures[2])
-                        push!(BestPoint, (x, y))
-                        break
-                    end
+                if parsed_value !== nothing
+                    push!(updateCountData, parsed_value)  # Use push! to add elements to Data
+                end
+            elseif occursin("Best behavior:", line)
+                m = Base.match(r"\[(-?\d+\.\d+),\s*(-?\d+\.\d+)\]", line)  # Use regex to extract two floats
+
+                if m !== nothing
+                    push!(BestPoint, (parse(Float64, m.captures[1]), parse(Float64, m.captures[2])))
+
+                    break
                 end
             end
         end
     end
 end
 
-fig = Figure()  # Add this line to define fig
+if isempty(updateCountData)
+    println("updateCountData is empty. Skipping color mapping and plotting.")
+    exit(1)
+else
+    println("updateCountData: ", length(updateCountData))
+end
 
-ax = [Axis(
-    fig[1, 1],
-    limits = ((LOW, UPP), (LOW, UPP)),
-    xlabel = L"b_1",
-    ylabel = L"b_2",
-    width  = 500,
-    height = 500
-),
-Axis(
-    fig[1, 3],
-    limits = ((LOW * closeup, UPP * closeup), (LOW * closeup, UPP * closeup)),
-    xlabel = L"b_1",
-    ylabel = L"b_2",
-    width  = 500,
-    height = 500
-)]
 
-if !isempty(updateCountData)
-    colormap = cgrad(:heat)
+filepath = if dimention == "test"
+    dir = "./result/testdata/"
+
+    if !isdir(dir)
+        error("Directory $dir does not exist.")
+    end
     
-    Colorbar(
-        fig[1, 2],
-        limits = (0, maximum(updateCountData)),
-        ticks=(0:maximum(updateCountData)/4:maximum(updateCountData), string.([0, "", "", "", maximum(updateCountData)])),
-        colormap = colormap,
-        label = "Update frequency"
-    )
-    Colorbar(
-        fig[1, 4],
-        limits = (0, maximum(updateCountData)),
-        ticks=(0:maximum(updateCountData)/4:maximum(updateCountData), string.([0, "", "", "", maximum(updateCountData)])),
-        colormap = colormap,
-        label = "Update frequency"
-    )
+    [path for path in readdir(dir) if occursin("test-", path) && occursin("behavior-", path)]
+else
+    dir = "./result/$(method_name)/$(function_name)/"
+
+    if !isdir(dir)
+        error("Directory $dir does not exist.")
+    end
+
+    [path for path in readdir(dir) if occursin("behavior-$(method_name)-$(map_name)-$(function_name)-$(dimention)-$(filedate)", path)]
+end
+
+if occursin(".dat", filepath[end])
+    println("Reading: ", joinpath(dir, filepath[end]))
+
+    open(joinpath(dir, filepath[end]), "r") do io
+        reading_data = false
+        border_count = 0
+        
+        for (k, line) in enumerate(eachline(io))
+            if occursin("=", line)
+                border_count += 1
+
+                if border_count == 2
+                    reading_data = true
+
+                    continue
+                end
+            end
+            
+            if reading_data
+                m = Base.match(r"\[(-?\d+\.\d+),\s*(-?\d+\.\d+)\]", line)  # Use regex to extract two floats
+
+                if m !== nothing
+                    push!(individualData, (parse(Float64, m.captures[1]), parse(Float64, m.captures[2])))
+                end
+            end
+        end
+    end
+end
+
+if isempty(individualData)
+    println("individualData is empty. Skipping color mapping and plotting.")
+    exit(1)
+else
+    println("individualData: ", length(individualData))
+end
+
+
+filepath = if dimention == "test"
+    dir = "./result/testdata/"
+
+    if !isdir(dir)
+        error("Directory $dir does not exist.")
+    end
+    
+    [path for path in readdir(dir) if occursin("test-", path) && occursin("fitness-test-", path)]
+else
+    dir = "./result/$(method_name)/$(function_name)/"
+
+    if !isdir(dir)
+        error("Directory $dir does not exist.")
+    end
+
+    [path for path in readdir(dir) if occursin("fitness-noise-$(method_name)-$(map_name)-$(function_name)-$(dimention)-$(filedate)", path)]
+end
+
+if occursin(".dat", filepath[end])
+    println("Reading: ", joinpath(dir, filepath[end]))
+
+    open(joinpath(dir, filepath[end]), "r") do io
+        reading_data = false
+        border_count = 0
+        
+        for (k, line) in enumerate(eachline(io))
+            if occursin("=", line)
+                border_count += 1
+
+                if border_count == 2
+                    reading_data = true
+
+                    continue
+                end
+            end
+            
+            if reading_data
+                parsed_value = tryparse(Float64, line)
+                
+                if parsed_value !== nothing
+                    push!(fitnessData, fitness(parsed_value))
+                end
+            end
+        end
+    end
+end
+
+if isempty(fitnessData)
+    println("fitnessData is empty. Skipping color mapping and plotting.")
+    exit(1)
+else
+    println("fitnessData: ", length(fitnessData))
+end
+
+
+for iter in ["FitnessValue"]
+    colormap = if iter == "UpdateFrequency"
+        cgrad(:heat)
+    else
+        cgrad(:viridis)
+    end
+
+    fig = Figure()
+
+    ax = [Axis(
+        fig[1, 1],
+        limits = ((LOW, UPP), (LOW, UPP)),
+        xlabel = L"b_1",
+        ylabel = L"b_2",
+        width  = 500,
+        height = 500
+    ),
+    Axis(
+        fig[1, 3],
+        limits = ((LOW * closeup, UPP * closeup), (LOW * closeup, UPP * closeup)),
+        xlabel = L"b_1",
+        ylabel = L"b_2",
+        width  = 500,
+        height = 500
+    )]
 
     voronoiplot!(
         ax[1],
@@ -168,99 +294,87 @@ if !isempty(updateCountData)
         show_generators = false,
         clip = (LOW * closeup, UPP * closeup, LOW * closeup, UPP * closeup)
     )
-else
-    println("updateCountData is empty. Skipping color mapping and plotting.")
 
-    exit(1)
-end
+    resize_to_layout!(fig)
 
-resize_to_layout!(fig)
-
-filepath = if ARGS[1] == "test"
-    dir = "./result/testdata/"
-    if !isdir(dir)
-        error("Directory $dir does not exist.")
-    end
-    
-    [path for path in readdir(dir) if occursin("test-", path) && occursin("behavior-", path)]
-else
-    dir = "./result/$(method_name)/$(function_name)/"
-    if !isdir(dir)
-        error("Directory $dir does not exist.")
-    end
-
-    [path for path in readdir(dir) if occursin("behavior-$(filedate)-$(method_name)-$(map_name)-$(function_name)-$(ARGS[1])", path)]
-end
-
-Data = Vector{Tuple{Float64, Float64}}()  # Change the type to Vector{Tuple{Float64, Float64}}
-
-for (i, f) in enumerate(filepath) # Change this line to iterate over readdir(dir) directly
-    if occursin(".dat", f)
-        open(joinpath(dir, f), "r") do io  # Use joinpath to construct the full path
-            reading_data = false # ボーダーライン検出用フラグ
-            border_count = 0  # ボーダーラインのカウント
-            
-            for (k, line) in enumerate(eachline(io)) # ファイルを1行ずつ読み込む
-                if occursin("=", line) # ボーダーラインを検出
-                    border_count += 1
-                    if border_count == 2 # 2つ目のボーダーラインに到達したらデータ読み取り開始
-                        reading_data = true
-                        continue
-                    end
-                end
-                
-                if reading_data
-                    m = Base.match(r"\[(-?\d+\.\d+),\s*(-?\d+\.\d+)\]", line)  # Use regex to extract two floats
-                    if m !== nothing
-                        x, y = parse(Float64, m.captures[1]), parse(Float64, m.captures[2])
-
-                        push!(Data, (x, y))
-                    end
-                end
-            end
+    scatter!(
+        ax[1],
+        [d[1] for d in individualData],
+        [d[2] for d in individualData],
+        marker = :circle, 
+        markersize =  7, 
+        color = if iter == "UpdateFrequency"
+            [(colormap[clamp(round(Int, ((fit - minimum(updateCountData)) / (maximum(updateCountData) - minimum(updateCountData))) * (length(colormap) - 1) + 1), 1, length(colormap))], 0.5 * ((fit - minimum(updateCountData)) / (maximum(updateCountData) - minimum(updateCountData)))^(1/2) + 0.4) for fit in updateCountData]
+        else
+            [(colormap[round(Int, fit * 255 + 1)], 0.5 * (fit^(1/2) + 0.4)) for fit in fitnessData]
         end
+    )
+    scatter!(
+        ax[2], 
+        [d[1] for d in individualData],
+        [d[2] for d in individualData],
+        marker = :circle, 
+        markersize = 14, 
+        color = if iter == "UpdateFrequency"
+            [(colormap[clamp(round(Int, ((fit - minimum(updateCountData)) / (maximum(updateCountData) - minimum(updateCountData))) * (length(colormap) - 1) + 1), 1, length(colormap))], 0.5 * ((fit - minimum(updateCountData)) / (maximum(updateCountData) - minimum(updateCountData)))^(1/2) + 0.4) for fit in updateCountData]
+        else
+            [(colormap[round(Int, fit * 255 + 1)], 0.5 * (fit^(1/2) + 0.4)) for fit in fitnessData]
+        end
+    )
+
+    scatter!(ax[1], BestPoint, marker = :star5, markersize = 15, color = :orange)
+    scatter!(ax[2], BestPoint, marker = :star5, markersize = 25, color = :orange)
+
+    Colorbar(
+        fig[1, 2],
+        limits = (0, maximum(updateCountData)),
+        ticks = if iter == "UpdateFrequency"
+            (0:maximum(updateCountData)/4:maximum(updateCountData), string.([0, "", "", "", maximum(updateCountData)]))
+        else
+            (0:0.25:1.0, string.([0.00, 0.25, 0.50, 0.75, 1.00]))
+        end,
+        colormap = colormap,
+        label = if iter == "UpdateFrequency"
+            "Update frequency"
+        else
+            "Fitness value"
+        end
+    )
+    Colorbar(
+        fig[1, 4],
+        limits = (0, maximum(updateCountData)),
+        ticks = if iter == "UpdateFrequency"
+            (0:maximum(updateCountData)/4:maximum(updateCountData), string.([0, "", "", "", maximum(updateCountData)]))
+        else
+            (0:0.25:1.0, string.([0.00, 0.25, 0.50, 0.75, 1.00]))
+        end,
+        colormap = colormap,
+        label = if iter == "UpdateFrequency"
+            "Update frequency"
+        else
+            "Fitness value"
+        end
+    )
+
+    poly!(
+        ax[1], 
+        Rect(-UPP * closeup, -UPP * closeup, UPP * 2 * closeup, UPP * 2 * closeup),
+        strokecolor = :blue,
+        color = (:blue, 0.0),
+        strokewidth = 1.0
+    )
+
+    resize_to_layout!(fig)
+
+    if !isdir("result/pdf")
+        mkdir("result/pdf")
     end
-end
 
-
-scatter!(
-    ax[1],
-    [d[1] for d in Data],
-    [d[2] for d in Data],
-    marker = :circle, 
-    markersize =  7, 
-    color = [(colormap[round(Int, (fit - minimum(updateCountData)) / (maximum(updateCountData) - minimum(updateCountData)) * (length(colormap) - 1) + 1)], 0.5 * ((fit - minimum(updateCountData)) / (maximum(updateCountData) - minimum(updateCountData)))^(1/2) + 0.4) for fit in updateCountData]
-)
-scatter!(
-    ax[2], 
-    [d[1] for d in Data],
-    [d[2] for d in Data],
-    marker = :circle, 
-    markersize = 14, 
-    color = [(colormap[round(Int, (fit - minimum(updateCountData)) / (maximum(updateCountData) - minimum(updateCountData)) * (length(colormap) - 1) + 1)], 0.3 * ((fit - minimum(updateCountData)) / (maximum(updateCountData) - minimum(updateCountData)))^(1/2) + 0.6) for fit in updateCountData]
-)
-
-scatter!(ax[1], BestPoint, marker = :star5, markersize = 20, color = :orange)
-scatter!(ax[2], BestPoint, marker = :star5, markersize = 50, color = :orange)
-
-poly!(
-    ax[1], 
-    Rect(-UPP * closeup, -UPP * closeup, UPP * 2 * closeup, UPP * 2 * closeup),
-    strokecolor = :blue,
-    color = (:blue, 0.0),
-    strokewidth = 1.0
-)
-
-resize_to_layout!(fig)
-
-if !isdir("result/$(ARGS[3])")
-    mkdir("result/$(ARGS[3])")
-end
-
-if ARGS[1] == "test"
-    println("Saved: result/testdata/pdf/cvt-testdata.pdf")
-    save("result/testdata/pdf/cvt-testdata.pdf", fig)
-else
-    println("Saved: result/$(ARGS[3])/$(ARGS[4])-$(ARGS[2])-$(ARGS[1]).pdf")
-    save("result/$(ARGS[3])/$(ARGS[4])-$(ARGS[2])-$(ARGS[1]).pdf", fig)
+    if dimention == "test"
+        println("Saved: result/testdata/pdf/cvt-testdata-$(iter).pdf")
+        save("result/testdata/pdf/cvt-testdata-$(iter).pdf", fig)
+    else
+        println("Saved: result/pdf/$(function_name)-$(method_name)-$(dimention)-$(iter).pdf")
+        save("result/pdf/$(function_name)-$(method_name)-$(dimention)-$(iter).pdf", fig)
+    end
 end
