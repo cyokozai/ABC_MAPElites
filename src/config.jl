7725bc73b4b00cbf57128b2479201bc13879cf5e
cyokozai <yuu4suke27@outlook.jp>
2025-01-16 10:33:39 +0000

    フィットネスノイズの設定を有効化しました。

--- src/config.jl
+++ src/config.jl
@@ -41,7 +41,7 @@ CONV_FLAG = false
 EPS       = 1e-6
 
 # Fitness noise | 'true' is available when you want to add the noise to the fitness.
-FIT_NOISE = false
+FIT_NOISE = true
 
 # Noise probability | Default: 0.10
 p_noise   = 0.10
